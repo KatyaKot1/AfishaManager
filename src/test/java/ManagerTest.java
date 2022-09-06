a64fd1ecca42e774940536d337a3c81bebda04dd
Екатерина Бакланова <ekakotelevskaya@yandex.ru>
2022-09-06 14:10:18 +0300

    new tests

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -69,6 +69,76 @@ public class ManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldReturnFilmsIfThereAreMoreThenLimit() {
+        Manager manager = new Manager(5);
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film10",
+                "film9",
+                "film8",
+                "film7",
+                "film6"};
+        assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void shouldReturnFilmsIfThereAreLessThenLimit() {
+        Manager manager = new Manager();
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film5",
+                "film4",
+                "film3",
+                "film2",
+                "film1"
+        };
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void getShouldReturnResultFilmsIfThereIsUpToLimit() {
+        Manager manager = new Manager();
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"film10",
+                "film9",
+                "film8",
+                "film7",
+                "film6",
+                "film5",
+                "film4",
+                "film3",
+                "film2",
+                "film1"
+        };
+        assertArrayEquals(expected, actual);
+    }
 }
 
 
